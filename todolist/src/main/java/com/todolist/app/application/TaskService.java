53274c0fe7e9bd0bfc10a942a7b1ae50583bc915
Andrés Felipe Trujillo <andres.trujillo.1@gmail.com>
2026-09-25 14:17:03 +0200

    feat: faker

--- todolist/src/main/java/com/todolist/app/application/TaskService.java
+++ todolist/src/main/java/com/todolist/app/application/TaskService.java
@@ -6,6 +6,7 @@ import com.todolist.app.domain.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
 @Service
--- todolist/src/test/java/com/todolist/app/TodolistApplicationTests.java
+++ todolist/src/test/java/com/todolist/app/TodolistApplicationTests.java
@@ -1,14 +1,15 @@
 package com.todolist.app;
 
+import com.github.javafaker.Faker;
 import com.todolist.app.application.TaskService;
 import com.todolist.app.domain.entity.Task;
 import com.todolist.app.domain.repository.TaskRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureDataSourceInitialization;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class TodolistApplicationTests {
@@ -35,13 +36,36 @@ class TodolistApplicationTests {
     @Test
     void saveTaskInDataBase() {
 
+        Faker faker = new Faker();
         Task task = new Task();
 
-        task.setTitle("Title Task Test");
-        task.setDescription("Description Task");
+
+        task.setTitle(faker.name().title());
+        task.setDescription(faker.lorem().paragraph(3));
+        task.setCompleted(false);
+
+        Task taskCreated = taskService.createTask(task);
+
+        assertNotNull(taskCreated.getId());
 
 
+    }
 
+    @Test
+    void save1000TasksInDataBase() {
+
+        Task task = new Task();
+        Faker faker = new Faker();
+
+        for (int i = 0; i < 1000; i++) {
+
+            task.setTitle(faker.name().title());
+            task.setDescription(faker.lorem().paragraph(2));
+            task.setCompleted(false);
+
+            taskService.createTask(task);
+
+        }
 
     }
 
